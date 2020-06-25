a932609d6bbbe86d8766c196db6767adc8773316
David Rodríguez <deivid.rodriguez@riseup.net>
2020-06-25 13:11:29 +0200

    Make sure tests at least run
    Minitest::Spec doesn't let you use `gem` as a `let` variable.

--- spec/ruby_maven_spec.rb
+++ spec/ruby_maven_spec.rb
@@ -16,21 +16,21 @@ describe RubyMaven do
     end
   end
 
-  let :gem do
+  let :gem_name do
     v = Maven::Ruby::VERSION
     v += '-SNAPSHOT' if v =~ /[a-zA-Z]/
     "pkg/ruby-maven-#{v}.gem"
   end
 
   it 'pack the gem' do
-    FileUtils.rm_f gem
+    FileUtils.rm_f gem_name
     CatchStdout.exec do
       # need newer jruby version
       RubyMaven.exec( '-Dverbose', 'package', '-Djruby.version=1.7.24' )
     end
     #puts CatchStdout.result
     CatchStdout.result.must_match /mvn -Dverbose package/
-    File.exists?( gem ).must_equal true
+    File.exists?( gem_name ).must_equal true
     File.exists?( '.mvn/extensions.xml' ).must_equal true
     File.exists?( '.mvn/extensions.xml.orig' ).wont_equal true
   end
