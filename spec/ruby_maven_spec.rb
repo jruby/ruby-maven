acc37330345b254d9147ca852e2f96a174a8050a
David Rodriguez <deivid.rodriguez@riseup.net>
2021-10-12 19:23:43 +0200

    Fix Minitest 6 deprecations

--- spec/ruby_maven_spec.rb
+++ spec/ruby_maven_spec.rb
@@ -10,9 +10,9 @@ describe RubyMaven do
       CatchStdout.exec do
         RubyMaven.exec( '--version' )
       end
-      CatchStdout.result.must_match /Polyglot Maven Extension 0.4.4/
+      _(CatchStdout.result).must_match /Polyglot Maven Extension 0.4.4/
       xml = File.read('.mvn/extensions.xml')
-      xml.must_equal "dummy\n"
+      _(xml).must_equal "dummy\n"
     end
   end
 
@@ -29,10 +29,10 @@ describe RubyMaven do
       RubyMaven.exec( '-Dverbose', 'package', '-Djruby.version=9.3.0.0' )
     end
     #puts CatchStdout.result
-    CatchStdout.result.must_match /mvn -Dverbose package/
-    File.exists?( gem_name ).must_equal true
-    File.exists?( '.mvn/extensions.xml' ).must_equal true
-    File.exists?( '.mvn/extensions.xml.orig' ).wont_equal true
+    _(CatchStdout.result).must_match /mvn -Dverbose package/
+    _(File.exists?( gem_name )).must_equal true
+    _(File.exists?( '.mvn/extensions.xml' )).must_equal true
+    _(File.exists?( '.mvn/extensions.xml.orig' )).wont_equal true
   end
   
 end
