d7ff8f5c464eadf1e065c71d343aa697accbc90c
David Rodríguez <deivid.rodriguez@riseup.net>
2021-11-07 17:52:41 +0100

    Rule out thread safety issue related the pwd/chdir

--- spec/maven_ruby_maven_spec.rb
+++ spec/maven_ruby_maven_spec.rb
@@ -5,6 +5,8 @@ describe Maven::Ruby::Maven do
 
   subject { Maven::Ruby::Maven.new }
 
+  let(:pkg) { File.expand_path("../pkg", __dir__) }
+
   it 'shows mvn commandline with verbose flag' do
     CatchStdout.exec do
       subject.exec( '-Dverbose', 'validate' )
@@ -16,8 +18,8 @@ describe Maven::Ruby::Maven do
   it 'takes declared jruby version' do
     begin
       subject.inherit_jruby_version '9.0.4.0'
-      subject.exec( '-X', 'initialize', '-l', 'pkg/log1.txt' )
-      _(File.read('pkg/log1.txt')).must_match /resolve jruby for version 9.0.4.0/
+      subject.exec( '-X', 'initialize', '-l', "#{pkg}/log1.txt" )
+      _(File.read("#{pkg}/log1.txt")).must_match /resolve jruby for version 9.0.4.0/
     ensure
       subject['jruby.version'] = nil
     end
@@ -26,14 +28,14 @@ describe Maven::Ruby::Maven do
   if defined? JRUBY_VERSION
     it 'inherits jruby version' do
       subject.inherit_jruby_version
-      subject.exec( '-X', 'initialize', '-l', 'pkg/log2.txt' )
-      _(File.read('pkg/log2.txt')).must_match /resolve jruby for version #{JRUBY_VERSION}/
+      subject.exec( '-X', 'initialize', '-l', "#{pkg}/log2.txt" )
+      _(File.read("#{pkg}/log2.txt")).must_match /resolve jruby for version #{JRUBY_VERSION}/
     end
   else
     it 'takes default jruby version with inherit jruby version' do
       subject.inherit_jruby_version
-      subject.exec( '-X', 'initialize', '-l', 'pkg/log3.txt' )
-      _(File.read('pkg/log3.txt')).must_match /resolve jruby for version 9.3.1.0/
+      subject.exec( '-X', 'initialize', '-l', "#{pkg}/log3.txt" )
+      _(File.read("#{pkg}/log3.txt")).must_match /resolve jruby for version 9.3.1.0/
     end
   end
  end
