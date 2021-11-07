ecc4cb997d9713f8d4e611af6fccb546c95cf4a7
David Rodríguez <deivid.rodriguez@riseup.net>
2021-11-07 18:28:12 +0100

    Use standard minitest utility for checking output
    Instead of relying on `CatchStdout` or passing the log file parameter to
    mvn.

--- spec/maven_ruby_maven_spec.rb
+++ spec/maven_ruby_maven_spec.rb
@@ -8,18 +8,20 @@ describe Maven::Ruby::Maven do
   let(:pkg) { File.expand_path("../pkg", __dir__) }
 
   it 'shows mvn commandline with verbose flag' do
-    CatchStdout.exec do
+    out, _ = capture_subprocess_io do
       subject.exec( '-Dverbose', 'validate' )
     end
     subject.verbose = false
-    _(CatchStdout.result).must_match /mvn -Dverbose validate/
+    _(out).must_match /mvn -Dverbose validate/
   end
 
   it 'takes declared jruby version' do
     begin
       subject.inherit_jruby_version '9.0.4.0'
-      subject.exec( '-X', 'initialize', '-l', "#{pkg}/log1.txt" )
-      _(File.read("#{pkg}/log1.txt")).must_match /resolve jruby for version 9.0.4.0/
+      out, _ = capture_subprocess_io do
+        subject.exec( '-X', 'initialize')
+      end
+      _(out).must_match /resolve jruby for version 9.0.4.0/
     ensure
       subject['jruby.version'] = nil
     end
@@ -28,14 +30,18 @@ describe Maven::Ruby::Maven do
   if defined? JRUBY_VERSION
     it 'inherits jruby version' do
       subject.inherit_jruby_version
-      subject.exec( '-X', 'initialize', '-l', "#{pkg}/log2.txt" )
-      _(File.read("#{pkg}/log2.txt")).must_match /resolve jruby for version #{JRUBY_VERSION}/
+      out, _ = capture_subprocess_io do
+        subject.exec( '-X', 'initialize')
+      end
+      _(out).must_match /resolve jruby for version #{JRUBY_VERSION}/
     end
   else
     it 'takes default jruby version with inherit jruby version' do
       subject.inherit_jruby_version
-      subject.exec( '-X', 'initialize', '-l', "#{pkg}/log3.txt" )
-      _(File.read("#{pkg}/log3.txt")).must_match /resolve jruby for version 9.3.1.0/
+      out, _ = capture_subprocess_io do
+        subject.exec( '-X', 'initialize')
+      end
+      _(out).must_match /resolve jruby for version 9.3.1.0/
     end
   end
  end
--- spec/setup.rb
+++ spec/setup.rb
@@ -3,23 +3,3 @@ $LOAD_PATH.unshift File.expand_path( '../../lib', __FILE__ )
 ENV["MT_NO_PLUGINS"] = "true"
 
 require 'minitest/autorun'
-
-
-module CatchStdout
-
-  def self.exec
-    out = $stdout
-    err = $stderr
-    @result = StringIO.new
-    $stdout = @result
-    $stderr = @result
-    yield
-  ensure
-    $stdout = out
-    $stderr = err
-  end
-
-  def self.result
-    @result.string
-  end
-end
